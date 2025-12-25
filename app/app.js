19f62f47bf879fed9b855fc0835844494890b11e
MazumalaAja <programlia1108@gmail.com>
2025-12-26 05:35:25 +0700

    modify sidebar and add toggle

---
+++ app/app.js
@@ -0,0 +1,6 @@
+// Hilangkan sidebar...
+const toggle = document.querySelector("#toggle");
+const sidebar = document.querySelector("aside");
+toggle.addEventListener("click" , () => {
+     sidebar.classList.toggle("active");
+})
